31fff4db18e4e87383c5d8bf8976e394ae507bb1
iswantirahayuu <iswantirahayuningtiyas@gmail.com>
2023-12-22 18:04:45 +0700

    webgis

---
+++ javascript.js
@@ -0,0 +1,69 @@
+//menu
+var tombolMenu=$(".tombol-menu");
+var menu =$("nav .menu ul");
+
+function klikMenu(){
+tombolMenu.click(function(){
+    menu.toggle();
+});
+menu.click(function(){
+    menu.toggle();
+});
+}
+
+$(document).ready(function(){
+var width = $(window).width();
+if(width<990){
+    klikMenu();
+}
+})
+
+//check . lebar
+$(window).resize(function (){
+    var width = $(window).width();
+    if (width > 989) {
+    menu.css("display","block");
+        //display:block
+    } else {
+    menu.css("display","none");
+}
+klikMenu();
+});
+
+//backround efek scroll
+    $(document).ready(function(){
+    var scroll_pos=0;
+    $(document).scroll(function(){
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 0){
+            $("nav").addClass("putih");
+            $("nav img.hitam").hide();
+            $("nav img.putih").show();
+
+        } else{
+            $("nav").removeClass("putih");
+            $("nav img.hitam").show();
+            $("nav img.putih").hide();
+            
+        }
+    })
+});
+
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#thumbnail-carousel', {
+          fixedWidth: 100,
+          gap       : 10,
+          rewind    : true,
+          pagination: false,
+    } ).mount();
+  } );
+
+
+  var main = new Splide( '#main-carousel', {
+    type      : 'fade',
+    rewind    : true,
+    pagination: false,
+    arrows    : false,
+  } );
+
+  
